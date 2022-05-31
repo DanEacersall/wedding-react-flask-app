950b1d10a12d294298d47b12d5a572bdf928a024
Daniel Eacersall <daniel.eacersall@gmail.com>
2022-05-31 10:56:56 +1000

    added token

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -6,6 +6,8 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 export const Login = () => {
 const [data, setData] = useState({})
 const [code, setCode] = useState("");
+const token = sessionStorage.getItem("token");
+console.log(token)
 
 useEffect(() => {
     fetch("http://127.0.0.1:5000/time/1").then(data => data.json())
@@ -27,16 +29,21 @@ const handleClick = () => {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            "name": data.name,
+            "name": code,
             "id": data.id
         })
     }
     fetch('http://127.0.0.1:5000/token', opts)
     .then(resp => {
         if(resp.status === 200) return resp.json();
+        console.log("This came from the back end", resp)
+        if(resp.status === 401) return resp.json();
         else alert("There has been an error")
     })
-    .then()
+    .then(data => {
+        
+        sessionStorage.setItem("token", data.access_token)
+    })
     .catch(error => {
         console.error("ERRORRR!!!", error);
     })
@@ -49,10 +56,12 @@ const handleClick = () => {
   return (
     <div className="App-header">
         <h1>Enter Your Code</h1>
-        <div>
+        {(token && token!="" && token!=undefined) ? "You are logged in with this token" + token :
+        <div>    
             <input type="text" placeholder='CODE' value={code} onChange={(e) => setCode(e.target.value)} />
             <button onClick={handleClick}>Submit</button>
         </div>
+        }
       {data.name}
     </div>
   )
